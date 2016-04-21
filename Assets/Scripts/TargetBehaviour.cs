77317277385afde3769a9820024950216412e569
MartijnVanBuul <martijn.vanbuul@student.fontys.nl>
2016-04-21 15:09:39 +0200

    Added the movement of the targets

--- Assets/Scripts/TargetBehaviour.cs
+++ Assets/Scripts/TargetBehaviour.cs
@@ -2,19 +2,41 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class TargetBehaviour : MonoBehaviour {
+public class TargetBehaviour : MonoBehaviour
+{
+    public enum moveAxis{
+        x,
+        y,
+        z
+    }
 
     public int Score = 10;
+    public bool IsMoving = false;
+    public moveAxis MoveAxis;
+    public int MoveDistance = 10;
+    public float Speed = 2;
+
+    private int direction = 1;
+    private Vector3 intitialLocation;
+    private Vector3 targetLocation;
 
     private AudioSource myAudioSource;
     private List<GameObject> hitObjects = new List<GameObject>();
-	
+
     void Start()
     {
         myAudioSource = GetComponent<AudioSource>();
+
+        intitialLocation = transform.position;
     }
 
-	void OnCollisionEnter(Collision objectCollision)
+    void Update()
+    {
+        if (IsMoving)
+            moveTarget();
+    }
+
+    void OnCollisionEnter(Collision objectCollision)
     {
         GameObject collisionObject = objectCollision.gameObject;
 
@@ -28,5 +50,29 @@ public class TargetBehaviour : MonoBehaviour {
         }
 
         hitObjects.Add(collisionObject);
+    }
+
+    /// <summary>
+    /// Method for moving the target.
+    /// </summary>
+    private void moveTarget()
+    {
+        Debug.Log((int)MoveAxis);
+        switch ((int)MoveAxis)
+        {
+            case 0:
+                targetLocation = intitialLocation + new Vector3(MoveDistance * direction, 0, 0);
+                break;
+            case 1:
+                targetLocation = intitialLocation + new Vector3(0, MoveDistance * direction, 0);
+                break;
+            case 2:
+                targetLocation = intitialLocation + new Vector3(0, 0, MoveDistance * direction);
+                break;
+        }
+        transform.position = Vector3.MoveTowards(transform.position, targetLocation, ((Vector3.Distance(transform.position, targetLocation)) / Speed + Speed) * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetLocation) < 0.1f)
+            direction *= -1;
     }
 }
