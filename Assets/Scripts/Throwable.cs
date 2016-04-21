5d426d08ff19668cc10ec624cf9fbed36f6d06a2
Jim Sanders <j.sanders@student.fontys.nl>
2016-04-21 14:47:00 +0200

    Added InputManager and Throwable scripts. Also added a player prefab, which contains the camera and spawn location of the throwable.

---
+++ Assets/Scripts/Throwable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Throwable : MonoBehaviour {
+
+    private Rigidbody myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        myRigidbody.useGravity = false;
+    }
+
+    /// <summary>
+    /// Launch the object in the given direction with the given speed.
+    /// </summary>
+    /// <param name="force"></param>
+    /// <param name="speed"></param>
+    public void Throw(Vector3 direction, float speed)
+    {
+        Vector3 force = direction * speed;
+        myRigidbody.AddForce(force);
+        myRigidbody.useGravity = true;
+    }
+}
