a79ffcad43c4062a96d4835f9ea31e4506d1630a
MartijnVanBuul <martijn.vanbuul@student.fontys.nl>
2016-04-21 16:52:30 +0200

    Added checks for reloading and throwing

--- Assets/Scripts/HandMotion.cs
+++ Assets/Scripts/HandMotion.cs
@@ -9,6 +9,12 @@ public class HandMotion : MonoBehaviour {
     public delegate void ThrowActivate();
     public static event ThrowActivate OnThrowActivate;
 
+    public delegate void ThrowReload();
+    public static event ThrowReload OnThrowReload;
+
+    private bool isThrown;
+    private bool isReloaded;
+
     // Use this for initialization
     void Awake ()
     {
@@ -17,8 +23,26 @@ public class HandMotion : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (handsim.glove.Fingers[1] > 0.8 && handsim.glove.Fingers[2] > 0.8)
-            if(OnThrowActivate != null)
+        if (handsim.glove.Fingers[1] > 0.8 && handsim.glove.Fingers[2] > 0.8 && !isThrown)
+        {
+            if (OnThrowActivate != null)
+            {
                 OnThrowActivate();
-	}
+                isThrown = true;
+            }
+        }
+        else if (handsim.glove.Fingers[1] < 0.2 && handsim.glove.Fingers[2] < 0.2 && isReloaded)
+        {
+            if (OnThrowActivate != null)
+            {
+                OnThrowActivate();
+                isReloaded = true;
+            }
+        }
+        else
+        {
+            isThrown = false;
+            isReloaded = false;
+        }
+    }
 }
