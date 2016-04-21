6cdeb64051f9c71139c98ee47569d0a436a0145e
Jim Sanders <j.sanders@student.fontys.nl>
2016-04-21 21:45:22 +0200

    Added Firefly and Weaponrack interactable.

---
+++ Assets/Scripts/Interactables/Interactable_Firefly.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable_Firefly : Interactable {
+
+    [SerializeField]
+    private MeshRenderer hideRendererOnDeath;
+
+    [SerializeField]
+    private float destroyAfterSeconds = 5f;
+
+	// Use this for initialization
+	void Start () {
+        base.onColission = Die;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        FlyAround();
+	}
+
+    private void FlyAround()
+    {
+        //TODO Move around in a certain area within its spawn area.
+    }
+
+    private void Die()
+    {
+        StartCoroutine("HideAndDestroy");
+    }
+
+    private IEnumerator HideAndDestroy()
+    {
+        hideRendererOnDeath.enabled = false;
+
+        yield return new WaitForSeconds(destroyAfterSeconds);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Interactables/Interactable_WeaponRack.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable_WeaponRack : Interactable {
+
+    [SerializeField]
+    private Rigidbody[] weapons;
+
+    [SerializeField]
+    private float bumpIntensityMin = 25f;
+
+    [SerializeField]
+    private float bumpIntensityMax = 100f;
+
+    private bool hasCollided = false;
+
+	// Use this for initialization
+	void Start () {
+        base.onColission = ActivatePhysicsOnWeapons;
+	}
+
+    private void ActivatePhysicsOnWeapons()
+    {
+        if (!hasCollided)
+        {
+            foreach (Rigidbody weapon in weapons)
+            {
+                weapon.constraints = RigidbodyConstraints.None;
+                weapon.AddForce(GetRandomBumpForce());
+            }
+            hasCollided = true;
+        }        
+    }
+
+    private Vector3 GetRandomBumpForce()
+    {
+        float randomX = Random.Range(bumpIntensityMin, bumpIntensityMax);
+        float randomY = Random.Range(bumpIntensityMin, bumpIntensityMax);
+        float randomZ = Random.Range(bumpIntensityMin, bumpIntensityMax);
+
+        return new Vector3(randomX, randomY, randomZ);
+    }
+	
+}
