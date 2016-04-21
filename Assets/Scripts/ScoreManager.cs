a51847ebb03ec28a7e260a573e0293e72eeb4b34
MartijnVanBuul <martijn.vanbuul@student.fontys.nl>
2016-04-21 14:31:00 +0200

    Added targets

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+    public static ScoreManager instance;
+    public Text scoreText;
+
+    public static int totalScore;
+
+    void Awake () {
+        instance = this;
+	}
+	
+    /// <summary>
+    /// Method for adding points to the total score.
+    /// </summary>
+    /// <param name="points">The points that will be added to the total.</param>
+	public void addScore(int points)
+    {
+        totalScore += points;
+        scoreText.text = totalScore.ToString();
+    }
+}
