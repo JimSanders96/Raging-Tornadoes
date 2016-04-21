a4fed3c4fd22a8c2585620b7e433f20ca1d637f2
JimSanders96 <krijgdekak@gmail.com>
2016-04-21 16:56:39 +0200

    Added throw input

---
+++ Assets/Scripts/ViveThrowInput.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ViveThrowInput : MonoBehaviour {
+    [SerializeField]
+    private Transform viveController;
+    private Vector3 lastPosition = Vector3.zero;
+
+    public float DistanceSpeed { get; set; }
+
+    void Start()
+    {
+        DistanceSpeed = 0;
+    }
+ 
+    void FixedUpdate()
+    {
+        DistanceSpeed = (((viveController.transform.position - lastPosition).magnitude) / Time.deltaTime);
+        lastPosition = viveController.transform.position;
+    }
+}
